87e514b5deafadd97d1cdf24583cd9e1e16d8260
Oleksii <vasin.oleksii@gmail.com>
2025-09-18 17:03:31 +0200

    links to stage

--- src/components/homepage/stage.jsx
+++ src/components/homepage/stage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 
 import Card from "../common/card";
@@ -8,28 +9,30 @@ import "./styles/stage.css";
 const Stage = () => {
 	return (
 		<div className="stage">
-			<Card
-				icon={faGraduationCap}
-				title="Stage"
-				body={
-					<div className="stage-body">
-						<div className="stage-item">
-							<img
-								src="habitat.png"
-								alt="Habitat et Humanisme"
-								className="stage-image"
-							/>
-							<div className="stage-title">
-								Habitat et Humanisme
+			<Link to="/stages" className="stage-link">
+				<Card
+					icon={faGraduationCap}
+					title="Stage"
+					body={
+						<div className="stage-body">
+							<div className="stage-item">
+								<img
+									src="habitat.png"
+									alt="Habitat et Humanisme"
+									className="stage-image"
+								/>
+								<div className="stage-title">
+									Habitat et Humanisme
+								</div>
+								<div className="stage-subtitle">
+									Stagiaire Développement Web
+								</div>
+								<div className="stage-duration">2025</div>
 							</div>
-							<div className="stage-subtitle">
-								Stagiaire Développement Web
-							</div>
-							<div className="stage-duration">2025</div>
 						</div>
-					</div>
-				}
-			/>
+					}
+				/>
+			</Link>
 		</div>
 	);
 };
